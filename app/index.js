debed73ad968058916bdffde39bb933ad886a95b
MennaEbied <mennahesham253@gmail.com>
2024-12-05 01:10:19 +0200

    Implement Calculator in app

--- app/index.js
+++ app/index.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Calculator from "../utils/Calculator";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>heyy</Text>
+      <Calculator />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#333',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#202020",
+    justifyContent: "center",
   },
 });
